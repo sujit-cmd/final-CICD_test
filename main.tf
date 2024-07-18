32ff4d6857b1fba0b3dd97072a26f924685bfa25
sujit-student <sujit341@gmail.com>
2024-07-18 15:41:13 +0530

    added skipping [skip ci]

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "static-bucket" {
   name          = "github-cicd-demo-1000"
   location      = "US"
-  #Try ti keep as "US"
+  #Try ti keep as "USA"
   force_destroy = true
 }
