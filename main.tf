d72719005640cd9919d16038319c197a64fa7a28
sujit-student <sujit341@gmail.com>
2024-07-18 15:40:07 +0530

    try

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "static-bucket" {
   name          = "github-cicd-demo-1000"
   location      = "US"
-  #Try ti keep as "EU"
+  #Try ti keep as "US"
   force_destroy = true
 }
